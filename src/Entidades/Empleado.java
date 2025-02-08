4cfedeb8bc5716d312e2948627d33436841886a0
Ayrton <juanjo.fj2017@gmail.com>
2025-02-08 00:28:47 -0500

    Mi aporte.

---
+++ src/Entidades/Empleado.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empleado {
+    private int IdEmpleado;
+    private String Cedula;
+    private String Nombre;
+    private String Apellido;
+    private String Telefono;
+    private String Direccion;
+
+    public Empleado() {
+    }
+
+    public Empleado(int IdEmpleado, String Cedula, String Nombre, String Apellido, String Telefono, String Direccion) {
+        this.IdEmpleado = IdEmpleado;
+        this.Cedula = Cedula;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Telefono = Telefono;
+        this.Direccion = Direccion;
+    }
+
+    public int getIdEmpleado() {
+        return IdEmpleado;
+    }
+
+    public void setIdEmpleado(int IdEmpleado) {
+        this.IdEmpleado = IdEmpleado;
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    
+}
